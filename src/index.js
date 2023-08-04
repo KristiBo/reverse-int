2e4aa38367d6151ee4d11587c801a8904d9447f1
KristiBo <kristina.boroh@gmail.com>
2023-08-04 16:27:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return n > 0
+        ? n.toString().split('').reverse().join('')
+        : Math.abs(n).toString().split('').reverse().join('');
+};
